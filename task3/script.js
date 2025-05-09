901b6554a099b8390fa958bd9739f5c691772739
vishakhabeknalkar <sandhyabeknalkar62@gmail.com>
2025-05-09 10:57:12 +0530

    Add files via upload

---
+++ task3/script.js
@@ -0,0 +1,17 @@
+function append(value) {
+    document.getElementById("display").value += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById("display").value = '';
+  }
+  
+  function calculate() {
+    try {
+      let result = eval(document.getElementById("display").value);
+      document.getElementById("display").value = result;
+    } catch {
+      document.getElementById("display").value = "Error";
+    }
+  }
+  
